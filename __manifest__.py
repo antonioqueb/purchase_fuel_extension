d7b84520b39fa2ebcd36cfa7f37d52254e394a9e
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-10-10 09:46:03 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,18 @@
 {
-    'name': 'Extensión de Órdenes de Compra para Combustible',
-    'version': '1.0',
-    'summary': 'Extiende las órdenes de compra para gestionar recepciones de combustible',
-    'description': 'Este módulo agrega campos y modelos adicionales al módulo de Compras para gestionar la recepción y control de combustible.',
-    'author': 'ALPHAQUEB CONSULTING S.A.S.',
-    'category': 'Purchases',
-    'depends': ['purchase', 'fleet', 'hr'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/purchase_order_view.xml',
+    "name": "Extensión de Órdenes de Compra para Combustible",
+    "version": "1.0",
+    "summary": "Extiende las órdenes de compra para gestionar recepciones de combustible",
+    "description": "Este módulo agrega campos y modelos adicionales al módulo de Compras para gestionar la recepción y control de combustible.",
+    "author": "Alphaqueb Consulting S.A.S.",
+    "category": "Purchases",
+    "website": "https://alphaqueb.com",
+    "license": "LGPL-3",
+    "depends": ["purchase"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/purchase_order_view.xml"
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "auto_install": False,
+    "application": False
 }
