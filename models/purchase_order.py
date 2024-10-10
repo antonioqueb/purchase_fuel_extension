9688b675cc6139611713992c6efb681190f7ea00
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-10-10 16:27:13 -0600

    Up

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,14 +1,19 @@
-# models\purchase_order.py
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    fecha_hora_recepcion = fields.Datetime(string='Fecha y Hora de Recepción')
     pipa = fields.Char(string='Pipa/Camión Cisterna')
     transportista = fields.Char(string='Transportista')
     chofer_name = fields.Char(string='Nombre del Chofer')
     chofer_license = fields.Char(string='Licencia del Chofer')
-    fecha_hora_recepcion = fields.Datetime(string='Fecha y Hora de Recepción')
+    sello_seguridad = fields.Char(string='Sello de Seguridad', size=20)
+    tipo_combustible = fields.Selection([
+        ('diesel', 'Diésel'),
+        ('gasolina_magna', 'Gasolina Magna'),
+        ('gasolina_premium', 'Gasolina Premium')
+    ], string='Tipo de Combustible')
     fuel_source = fields.Selection([
         ('compra_directa', 'Compra Directa'),
         ('subsidio_gubernamental', 'Subsidio Gubernamental'),
@@ -28,21 +33,9 @@ class PurchaseOrder(models.Model):
     temperatura_combustible = fields.Float(string='Temperatura del Combustible')
     temperatura_ambiente = fields.Float(string='Temperatura Ambiente')
     documentos_adjuntos = fields.Binary(string='Documentos Adjuntos')
-    sello_seguridad = fields.Char(string='Sello de Seguridad', size=20)
-    tipo_combustible = fields.Selection([
-        ('diesel', 'Diésel'),
-        ('gasolina_magna', 'Gasolina Magna'),
-        ('gasolina_premium', 'Gasolina Premium')
-    ], string='Tipo de Combustible')
-    proveedor_secundario_id = fields.Many2one('res.partner', string='Proveedor Secundario')
-    contrato_id = fields.Many2one('purchase.contract', string='Contrato')
     documentacion_regulatoria = fields.Binary(string='Documentación Regulatoria')
     fotos_recepcion = fields.Many2many('ir.attachment', string='Fotos de Recepción')
     firma_responsable = fields.Binary(string='Firma del Responsable')
-    comentarios_auditoria = fields.Text(string='Comentarios de Auditoría')
-    conformidad = fields.Boolean(string='Conformidad')
-    notas_adicionales = fields.Text(string='Notas Adicionales')
-    historial_inspecciones_ids = fields.One2many('fuel.inspection', 'purchase_order_id', string='Historial de Inspecciones')
 
     @api.depends('quantity_expected', 'quantity_received')
     def _compute_diferencia_merma(self):
